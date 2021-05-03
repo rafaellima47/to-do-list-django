6178a234da52160f20a59042a746a2cf39cbf9c4
Rafael Lima <47rafael.lima@gmail.com>
2021-05-03 20:17:22 -0300

    settings.py update

--- todolist/settings.py
+++ todolist/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'todolist.urls'
@@ -146,5 +147,4 @@ DATABASES = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 '''
